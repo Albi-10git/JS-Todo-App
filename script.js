5a7da9c9ada9e96f83f46e844bfb038f62f9d406
Albin_. <albinpeter212@gmail.com>
2025-07-29 22:45:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -55,6 +55,8 @@ function updateEditDate() {
     month: "short",
     day: "numeric",
     year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit"
   });
 
   localStorage.setItem("lastEdited", formatted);
